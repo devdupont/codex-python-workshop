0cbd8b5da35a13d2c161c732faa42fa396e5441b
flyinactor91 <michael@mdupont.com>
2019-08-18 19:02:25 -0400

    Post-presentation model

--- tpot-intro/pipeline.py
+++ tpot-intro/pipeline.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
 from tpot.builtins import StackingEstimator
-from xgboost import XGBRegressor
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,10 +11,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:-10.868745779772542
+# Average CV score on the training set was:-10.222908257128386
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=False, max_features=0.7000000000000001, min_samples_leaf=6, min_samples_split=10, n_estimators=100)),
-    XGBRegressor(learning_rate=0.1, max_depth=9, min_child_weight=8, n_estimators=100, nthread=1, objective="reg:squarederror", subsample=0.7000000000000001)
+    StackingEstimator(estimator=AdaBoostRegressor(learning_rate=0.1, loss="square", n_estimators=100)),
+    StackingEstimator(estimator=RandomForestRegressor(bootstrap=False, max_features=0.15000000000000002, min_samples_leaf=8, min_samples_split=9, n_estimators=100)),
+    RandomForestRegressor(bootstrap=False, max_features=0.3, min_samples_leaf=4, min_samples_split=7, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
