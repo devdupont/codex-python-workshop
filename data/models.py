da77ca975d0cf8d8edff2d8cc4a3d581dd08b6d7
flyinactor91 <michael@mdupont.com>
2019-08-25 12:22:41 -0400

    Add demo

--- data/models.py
+++ data/models.py
@@ -1,7 +1,13 @@
+"""
+https://docs.sqlalchemy.org/en/13/orm/tutorial.html
+"""
+
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, Column, String, Float, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
-db = create_engine('sqlite:///:crimes:', echo=True)
+db = create_engine('sqlite:///crimes.sqlite', echo=True)
 Base = declarative_base()
 
 class Crime(Base):
@@ -21,3 +27,36 @@ class Crime(Base):
     disposition = Column(String)
     latitude = Column(Float)
     longitude = Column(Float)
+
+    def __repr__(self):
+        return f"Crime occured at {self.timestamp}"
+
+def demo():
+    """
+    """
+    # Create tables
+    Base.metadata.create_all(db)
+
+    Session = sessionmaker(bind=db)
+    session = Session()
+
+    c = Crime(
+        id=1,
+        number="12345",
+        timestamp=datetime.utcnow(),
+        address="1",
+        latitude=10.1,
+        longitude=-11
+    )
+    print(c)
+
+    session.add(c)
+    session.commit()
+
+def populate_from_csv(filename: str):
+    """
+    """
+    pass
+
+if __name__ == "__main__":
+    demo()
